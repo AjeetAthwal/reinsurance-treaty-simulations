5af0c545c7053880de7a3d2e48280b07a61376bb
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2023-08-26 03:37:45 +0100

    add gitpages UI

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 loss distribution was created using ChatGPT - https://chat.openai.com/share/11b35000-1262-46e6-a03c-29d5d54f232a
 xol treaty was created using ChatGPT - https://chat.openai.com/share/bdfbf90d-36fb-4f36-b404-d66f7e05cdc9
 added enums for distribution type using ChatGPT - https://chat.openai.com/share/feb406df-b712-4bd9-a6b9-0766f668c885
+Added gitpages UI via ChatGPT - https://chat.openai.com/share/0be3a326-1105-4ba5-9376-6197ef0b4c45
---
+++ index.js
@@ -0,0 +1,64 @@
+const {DistributionFactory, DistributionType } = require("./loss");
+const {XoLTreaty, XoLTreatyLossGenerator } = require("./treaty")
+
+
+// Function to display output in the UI
+function displayOutput(output) {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = output;
+}
+
+// Example usage
+const generator = DistributionFactory.createGenerator(distributionType, distributionParameters);
+const xolTreaty = new XoLTreaty(attachment, limit);
+const xolGenerator = new XoLTreatyLossGenerator(generator, xolTreaty);
+
+const { grossLosses, netLosses } = xolGenerator.generateLossesWithTreaty(numLosses);
+
+// Generate formatted output
+const formattedOutput = `
+Gross losses: ${grossLosses.join(", ")}
+Net losses with XoL Treaty: ${netLosses.join(", ")}
+`;
+
+// Display the output in the UI
+displayOutput(formattedOutput);
+
+// ... Your existing JavaScript code ...
+
+// Function to calculate and display results
+function calculateResults() {
+    const distributionType = document.getElementById("distribution-type").value;
+    const mean = parseFloat(document.getElementById("mean").value);
+    const standardDeviation = parseFloat(document.getElementById("standard-deviation").value);
+    const min = parseFloat(document.getElementById("min").value);
+    const max = parseFloat(document.getElementById("max").value);
+    const attachment = parseFloat(document.getElementById("attachment").value);
+    const limit = parseFloat(document.getElementById("limit").value);
+    const numLosses = parseInt(document.getElementById("num-losses").value);
+
+    const generator = DistributionFactory.createGenerator(distributionType, {
+        mean: mean,
+        standardDeviation: standardDeviation,
+        min: min,
+        max: max
+    });
+    const xolTreaty = new XoLTreaty(attachment, limit);
+    const xolGenerator = new XoLTreatyLossGenerator(generator, xolTreaty);
+
+    const { grossLosses, netLosses } = xolGenerator.generateLossesWithTreaty(numLosses);
+
+    // Generate formatted output
+    const formattedOutput = `
+    Gross losses: ${grossLosses.join(", ")}
+    Net losses with XoL Treaty: ${netLosses.join(", ")}
+    `;
+
+    // Display the output in the UI
+    displayOutput(formattedOutput);
+}
+
+// Attach event listener to the calculate button
+const calculateButton = document.getElementById("calculate-button");
+calculateButton.addEventListener("click", calculateResults);
+
