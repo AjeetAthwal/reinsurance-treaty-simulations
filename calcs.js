2e7008b77a319ebcd87f3f6b237258be5f8aedc3
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2023-08-26 02:38:01 +0100

    add XoL treaty on top of loss distribution

--- calcs.js
+++ calcs.js
@@ -1,62 +1,111 @@
 class LossGenerator {
     constructor(parameters) {
-      this.parameters = parameters;
+        this.parameters = parameters;
     }
-  
+
     generateLoss() {
-      throw new Error("generateLoss method must be implemented in subclasses");
+        throw new Error("generateLoss method must be implemented in subclasses");
     }
-  
+
     generateLosses(numLosses) {
-      const losses = [];
-      for (let i = 0; i < numLosses; i++) {
-        losses.push(this.generateLoss());
-      }
-      return losses;
+        const losses = [];
+        for (let i = 0; i < numLosses; i++) {
+            losses.push(this.generateLoss());
+        }
+        return losses;
     }
-  }
-  
-  class NormalLossGenerator extends LossGenerator {
+}
+
+class NormalLossGenerator extends LossGenerator {
     generateLoss() {
-      const { mean, standardDeviation } = this.parameters;
-      const u1 = Math.random();
-      const u2 = Math.random();
-      const z0 = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
-      const lossValue = mean + standardDeviation * z0;
-      return Math.max(0, lossValue);
+        const { mean, standardDeviation } = this.parameters;
+        const u1 = Math.random();
+        const u2 = Math.random();
+        const z0 = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+        const lossValue = mean + standardDeviation * z0;
+        return Math.max(0, lossValue);
     }
-  }
-  
-  class UniformLossGenerator extends LossGenerator {
+}
+
+class UniformLossGenerator extends LossGenerator {
     generateLoss() {
-      const { min, max } = this.parameters;
-      return Math.random() * (max - min) + min;
+        const { min, max } = this.parameters;
+        return Math.random() * (max - min) + min;
     }
-  }
-  
-  class DistributionFactory {
+}
+
+class XoLTreaty {
+    constructor(attachment, limit) {
+        this.attachment = attachment;
+        this.limit = limit;
+    }
+
+    applyTreaty(loss) {
+        const cappedLoss = Math.min(loss - this.attachment, this.limit);
+        return Math.max(cappedLoss, 0);
+    }
+}
+
+class XoLTreatyLossGenerator extends LossGenerator {
+    constructor(generator, treaty) {
+        super(generator.parameters);
+        this.generator = generator;
+        this.treaty = treaty;
+    }
+
+    generateLoss() {
+        const originalLoss = this.generator.generateLoss();
+        return this.treaty.applyTreaty(originalLoss);
+    }
+
+    generateLossesWithTreaty(numLosses) {
+        const grossLosses = this.generator.generateLosses(numLosses);
+        const netLosses = grossLosses.map(loss => this.treaty.applyTreaty(loss));
+        return { grossLosses, netLosses };
+    }
+}
+
+
+class DistributionFactory {
     static createGenerator(distributionType, parameters) {
-      if (distributionType === "normal") {
-        return new NormalLossGenerator(parameters);
-      } else if (distributionType === "uniform") {
-        return new UniformLossGenerator(parameters);
-      } else {
-        throw new Error("Unsupported distribution type");
-      }
+        if (distributionType === "normal") {
+            return new NormalLossGenerator(parameters);
+        } else if (distributionType === "uniform") {
+            return new UniformLossGenerator(parameters);
+        } else {
+            throw new Error("Unsupported distribution type");
+        }
     }
-  }
-  
-  // Example usage
-  const distributionType = "normal"; // Choose either "normal" or "uniform"
-  const distributionParameters = {
+}
+
+function createXoLTreatyLossGenerator(distributionType, distributionParameters, attachment, limit, numLosses) {
+    const xolTreaty = new XoLTreaty(attachment, limit);
+    const generator = DistributionFactory.createGenerator(distributionType, distributionParameters);
+    const xolGenerator = new XoLTreatyLossGenerator({ parameters: generator.parameters }, xolTreaty);
+
+    const grossLosses = generator.generateLosses(numLosses);
+    const netLosses = xolGenerator.generateLosses(numLosses);
+
+    return { grossLosses, netLosses };
+}
+
+// Example usage
+const distributionType = "normal"; // Choose either "normal" or "uniform"
+const distributionParameters = {
     mean: 1000000,
     standardDeviation: 200000,
     min: 500000,
     max: 1500000,
-  };
-  const numLosses = 1000;
-  
-  const generator = DistributionFactory.createGenerator(distributionType, distributionParameters);
-  const generatedLosses = generator.generateLosses(numLosses);
-  console.log("Generated losses:", generatedLosses);
-  
+};
+const attachment = 500000;
+const limit = 500000;
+const numLosses = 1000;
+
+const generator = DistributionFactory.createGenerator(distributionType, distributionParameters);
+const xolTreaty = new XoLTreaty(attachment, limit);
+const xolGenerator = new XoLTreatyLossGenerator(generator, xolTreaty);
+
+const { grossLosses, netLosses } = xolGenerator.generateLossesWithTreaty(numLosses);
+
+console.log("Gross losses:", grossLosses);
+console.log("Net losses with XoL Treaty:", netLosses);
